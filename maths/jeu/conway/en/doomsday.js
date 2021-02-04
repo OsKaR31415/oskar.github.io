88272dc579153ea919329645e44671ce0c42df6a
OsKaR31415 <76111415+OsKaR31415@users.noreply.github.com>
2021-02-05 00:12:08 +0100

    working on computer.js

--- maths/jeu/conway/en/doomsday.js
+++ maths/jeu/conway/en/doomsday.js
@@ -2,43 +2,61 @@ input = document.getElementById("input_date");
 
 DAYS = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
 
-function toDate(n){
-  // TODO
-    return "19/01/2021";
-}
-
-function toDay(n){
-  // TODO
-    return 1
-}
-
-// pour stocker ta date secrète 
+// pour stocker ta date secrète
 
 var secretDate;
 
-function random_date() {
-    secretDate = randomDate(new Date(2013, 1, 1), new Date(2013, 12, 31));
+function aff_random_date() {
+    secretDate = random_date(new Date(2013, 1, 1), new Date(2013, 12, 31));
     aff = document.getElementById("aff");
     aff.innerText = secretDate.toString();
 }
 
-function randomDate(start, end) {
+function random_date(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
+function secret_day_number() {
+    return secretDate.getDay();
+}
+
+var day_int = {
+    "lundi": 1,
+    "mardi": 2,
+    "mercredi": 3,
+    "jeudi": 4,
+    "vendredi": 5,
+    "samedi": 6,
+    "dimanche": 0,
+
+    "monday": 1,
+    "tuesday": 2,
+    "wedesday": 3,
+    "thursday": 4,
+    "friday": 5,
+    "saturday": 6,
+    "sunday": 0
+}
+
+function day_to_int(day) {
+    day = day.toLowerCase();
+
+    return day_int[day];
+}
+
 function check_date() {
-  let day_index = input.value
+    let day = day_to_int(input.value);
 
-  if (secretDate.good(day_index)){
-      alert("c'est la bonne réponse !");
-  }
-  else {
-    alert("mauvaise réponse")
-  }
+    let actual_day = secret_day_number(secretDate);
 
-  alert("la date secrète a changé")
+    if (day == actual_day){
+        alert("c'est la bonne réponse !");
+    }
+    else {
+        alert("mauvaise réponse");
+    }
 
-  //secretDate.update()
+    //secretDate.update()
 }
 
 //secretDate.update()
