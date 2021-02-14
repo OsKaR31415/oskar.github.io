7b6de85ad689aa46ce5c32bdd7c18c2f11e481a0
OsKaR31415 <76111415+OsKaR31415@users.noreply.github.com>
2021-02-14 15:47:20 +0100

    U

--- maths/jeu/conway/en/doomsday.js
+++ maths/jeu/conway/en/doomsday.js
@@ -8,8 +8,8 @@ var secretDate;
 var aff;
 var input;
 // simplement enlever weekday pour cacher le jour.
-// const date_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-const date_options = {year: 'numeric', month: 'long', day: 'numeric'};
+// const date_options = {year: 'numeric', month: 'long', day: 'numeric'};
+const date_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 
 function aff_random_date() {
     secretDate = random_date(new Date(2013, 1, 1), new Date(2013, 12, 31));
